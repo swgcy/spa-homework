5f9e0e8ff6467a9938c3dbe48b408584e95ba7e8
swgcy <thecookiecrumbles722@gmail.com>
2017-05-10 03:13:37 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title = 'app works! by sawa';
+  time : Date = new Date();
+  text : string;
+  
+  ngOnInit(){
+    setInterval(() => {this.time = new Date();}, 1000);
+  }
+  
+ // constructor() {
+ //    setInterval(function () { this.time = (new Date()).toString(); }.bind(this), 1000);
+ //}
+
 }
